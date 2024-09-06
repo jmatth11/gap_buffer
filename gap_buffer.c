d228b992a1af5466506b1c921bde40d0f0aa8d36
Joshua Matthews <jmatth93@gmail.com>
2024-09-06 11:12:33 -0500

    Update gap_buffer.c
    conform to __nonnull attribute

--- gap_buffer.c
+++ gap_buffer.c
@@ -27,9 +27,6 @@ static bool resize_buffer(struct gap_buffer *gb) {
 }
 
 bool gap_buffer_init(struct gap_buffer *gb, size_t buf_size) {
-  if (gb == NULL) {
-    return false;
-  }
   if (buf_size <= 0) {
     buf_size = GAP_DEFAULT_BUFFER_SIZE;
   }
